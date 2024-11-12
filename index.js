a8d462741159f37366ebf16411f5c180a53c2413
0ibrahimM <meshkat271@gmail.com>
2024-11-12 01:27:33 -0800

    foor lop

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+
+
+ for (let i=1; i<=10; i++) {
+    console.log("ibrahim hossain");
+     console.log(i)
+}
+
+let marks=[12,21,54,45,87,89,78,65,42,32,];
+for (let i=0; i<=9; i++){
+    console.log(marks[i]);
+}
+let number=[120,201,501,150,420,320,123,33,35,77,99]
+for (let i=0; i<=10; i++){
+
+    if(number[i]%2 ==0)
+
+
+
+        console.log("the number is evend =" + number[i]);
+else {
+   console.log("tesh number is odd =" + number[i]);
+}
+
+
+}
